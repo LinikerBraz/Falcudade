77a77ee77f47428b5efecc0f576803c4056bb268
LinikerBraz <166751266+LinikerBraz@users.noreply.github.com>
2025-02-26 20:25:04 -0300

    ...

---
+++ prof-angelo/ex002/Livro.java
@@ -0,0 +1,9 @@
+
+class Livro{
+    String nome;
+    String autor;
+    int isbn;
+    int numeroDePaginas;
+    int anoDePublicacao;
+    boolean estaDiposnivel;
+}
---
+++ prof-angelo/ex002/Main.java
@@ -0,0 +1,18 @@
+public class  Main{
+    
+    public static void main(String args[]){
+
+        Livro l1 = new Livro();
+        l1.titulo = "Harry Potter v1";
+        l1.autor = "J. K. Rowling";
+        l1.isbn = "9781781103685";
+        l1.numeroDePaginas = 312;
+        li.anoDePublicacao = 2005;
+        li.estaDiposnivel = true;
+
+        System.out.println();
+        
+
+
+    }
+}
